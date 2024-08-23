c038e7802740d2eae726f46f0e43d2719707b5f2
Eliel NIKIEMA <elielnikiema16@gmail.com>
2024-08-23 02:29:20 +0000

    Ajustement de l'auth de l'admin

--- auth/login_admin.php
+++ auth/login_admin.php
@@ -3,8 +3,8 @@ pour (L'Admin et les Bibliothécaires)
 Notons que si la session du bibliothécaire est lancée il ne peux plus avoir
 à cette page de même pour l'administrateur
 -->
-<?php require "includes/admin_session.php" ?>
-<?php require "config/config.php" ?>
+<?php require "../includes/admin_session.php" ?>
+<?php require "../config/config.php" ?>
 
 <?php
 
@@ -38,7 +38,7 @@ Notons que si la session du bibliothécaire est lancée il ne peux plus avoir
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="assets/css/login_admin.css">
+    <link rel="stylesheet" href="../assets/css/login_admin.css">
 </head>
 <body>
     <div class="container">
