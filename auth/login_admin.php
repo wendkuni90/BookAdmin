ff7379c0dcdcdcd45303f1d208cb7c20b3ef1771
Eliel NIKIEMA <elielnikiema16@gmail.com>
2024-09-02 23:13:50 +0000

    test

--- auth/login_admin.php
+++ auth/login_admin.php
@@ -67,5 +67,22 @@ Notons que si la session du bibliothécaire est lancée il ne peux plus avoir
             </form>
         </div>
     </div>
+
+    <script>
+        document.getElementById('togglePassword').addEventListener('click', function() {
+            const passwordField = document.getElementById('ad_pass');
+            const icon = this;
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                passwordField.type = 'password';
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
+        });
+    </script>
+    
 </body>
 </html>
--- manage/setting.php
+++ manage/setting.php
@@ -9,3 +9,14 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Paramètres</title>
+</head>
+<body>
+	Pour le moment rien ne sera inplémenter ici!!!!
+</body>
+</html>
